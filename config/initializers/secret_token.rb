84ba0bf31b025525cc77e627daa28eabbc584ce9
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-02-04 14:10:48 -0500

    initial commit, basic routing/pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToastyTights::Application.config.secret_token = '98308da65df84bac9ded0da25715ac3b2eb4bec6d60126f6e9fd042b91a4e76d4bca59f11a07c7f5fe9c87bf45ddd3990fa6d5aeeb01cfb8b1b54648f85c4d32'
