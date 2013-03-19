37ff0bdbf1611bb8e514cd1fdcd9f0ed3c186941
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-03-19 15:36:13 -0400

    Fixing spelling error, adding :order_item factory to factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,18 @@
 FactoryGirl.define do
   factory :product do
-  	color "black"
+  	color "Black"
   	size "S"
   	in_stock 10
   	price 30
+  end
+
+  factory :order_item do
+  	cart_id 1
+  	product_id 1
+  end 
+
+
+  factory :cart do
+    add_product(1)
   end
 end
