af9088ca2d0395979a22b31a9bbd82fae74726bf
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-03-19 15:38:03 -0400

    Adding order_item validations, moving enough_in_stock def from product to order_item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,5 +3,13 @@ class OrderItem < ActiveRecord::Base
   belongs_to :cart
 
   attr_accessible :cart_id, :product_id, :quantity
-  								
+
+  validates :quantity, numericality: { greater_than: 0, less_than_or_equal_to: 10 }
+  
+  def enough_in_stock(order_item_quantity)
+    if order_item.quantity > product.in_stock
+      self.errors.add(:quantity, "Please try again with a smaller quantity")
+    end
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,9 +8,4 @@ class Product < ActiveRecord::Base
   validates :price, numericality: { greater_than_or_equal_to: 1 }
   validates :size, inclusion: { :in => %w(S M L Children's) }
 
- def enough_in_stock(order_item_quantity)
-    if order_item.quantity > product.in_stock
-      self.errors.add(:quantity, "Please try again with a smaller quantity")
-    end
-  end
 end
