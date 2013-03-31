9f464408b52c4c37330954f3c0427bca561b3c45
aceofbassgreg <gregory.kenenitz@gmail.com>
2013-03-31 00:07:11 -0400

    Separating static and shop page specs

---
+++ spec/requests/shop_page_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe "Shop Page" do
+  before { visit shop_path }
+
+  describe "when products are in-stock" do
+    before { click_link "Select Product" }
+    product = FactoryGirl.build(:product)
+ 
+    before do
+      select 'Black - S', from: '#order-item-product-id'
+      fill_in "Quantity", with: "1"
+      click_button "Add to Cart"
+    end
+
+    it { should have_content('Item Successfully Added!') }
+  end
+
+  # describe "when products are out of stock" do
+  #   before do
+  #     product.in_stock == 0
+  #     click_link "Select Product"
+  #   end
+
+  #   it { should_not have_content('Black - S') }
+  # end
+
+  # describe "when customer tries adding more products than are in-stock" do
+    
+  #   before do
+  #     fill_in "Quantity", with: 11
+  #     click_button "Add to Cart"
+  #   end
+
+  #   it { should have_content('error') }
+  #   it { should_not have_content('Item Successfully Added!') }
+  # end
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,47 +18,5 @@ describe "StaticPages" do
       page.should have_selector 'h3', text: "Please select size, color, and quantity:"
     end
   end
-
-  # describe "Shop Page" do
-  #   before { visit shop_path }
-
-  #   describe "when products are in-stock" do
-  #     before { click_link "Select Product" }
-  #     product = FactoryGirl.build(:product)
- 
-  #     it { should have_selector 'option', text: "Small Black" }
-  #     end
-        
-  #     describe "adding available products to cart" do
-  #       before do
-  #         select 'Option', from: 'Select Box'
-  #         fill_in "Quantity", with: "1"
-  #         click_button "Add to Cart"
-  #       end
-
-  #       it { should_not have_content('error') }
-  #       it { should have_content('Item Successfully Added!') }
-  #     end
-  #   end
-
-  #   describe "when products are out of stock" do
-  #     #set quantity of small black to 0
-  #     before { click_link "Select Product" }
-
-  #     it { should_not have_content('Small Black') }
-  #   end
-
-  #   describe "when customer tries adding more products than are in-stock" do
-    
-  #     before do
-  #       fill_in "Quantity", with: 11
-  #       click_button "Add to Cart"
-  #     end
-
-  #     it { should have_content('error') }
-  #     it { should_not have_content('Item Successfully Added!') }
-  #   end
-  #   #     pending expect(click_button "Add to Cart").to change().by 1
-  # end
 end
 
